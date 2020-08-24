b71e34385f193a639260051d7d9e875339c37384
Martin Janiczek <martin@janiczek.cz>
2020-08-25 01:58:44 +0200

    Whoops

--- src/Elm/Kernel/Float.js
+++ src/Elm/Kernel/Float.js
@@ -7,8 +7,8 @@ var _Float_fromBytes = F2(function(hi,lo)
     // hi, lo : Int(0..0xFFFFFFFF)
     var buffer = new ArrayBuffer(8);
     var view = new DataView(buffer);
-    view.setInt32(0, hi);
-    view.setInt32(1, lo);
+    view.setUint32(0, hi);
+    view.setUint32(1, lo);
     return view.getFloat64(0);
 });
 

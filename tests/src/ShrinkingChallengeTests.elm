fbb497ee07bf9b04edc294dea7a4c2e099263f7b
Martin Janiczek <martin@janiczek.cz>
2020-08-28 01:09:40 +0200

    Implement binHeap shrinker challenge

--- tests/src/ShrinkingChallengeTests.elm
+++ tests/src/ShrinkingChallengeTests.elm
@@ -47,6 +47,7 @@ largeUnionList =
     simplifiesTowardsMany
         "large union list"
         [ [ [ 0, 1, 2, 3, 4 ] ]
+        , [ [ 0, 1, 2, 3, -1 ] ]
         , [ [ 1, 2, -2, 0, -1 ] ]
         , [ [ 2, -1, -2, 0, 1 ] ]
         ]
@@ -259,21 +260,79 @@ difference3 =
 -}
 binHeap : Test
 binHeap =
-    -- Given: Heap = Heap Int (Maybe Heap) (Maybe Heap)
-    -- Property: \heap ->
-    --   let
-    --     l1 = toList heap
-    --     l2 = wrongToSortedList heap
-    --   in
-    --   (l2 == List.sort l2) && (List.sort l1 == l2)
-    -- Shrinks towards:
-    --   Heap 0
-    --     Nothing
-    --     (Heap 0
-    --        (Heap 0 Nothing Nothing)
-    --        (Heap 1 Nothing Nothing)
-    --     )
-    todo "binHeap"
+    let
+        heapFuzzer : Int -> Fuzzer Heap
+        heapFuzzer depth =
+            if depth <= 0 then
+                Fuzz.map (\i -> Heap i Nothing Nothing) Fuzz.int
+
+            else
+                Fuzz.map3 Heap
+                    Fuzz.int
+                    (Fuzz.maybe (heapFuzzer (depth - 1)))
+                    (Fuzz.maybe (heapFuzzer (depth - 1)))
+
+        toList : Heap -> List Int
+        toList heap =
+            let
+                go : List Int -> List Heap -> List Int
+                go acc stack =
+                    case stack of
+                        [] ->
+                            List.reverse acc
+
+                        (Heap n left right) :: hs ->
+                            go
+                                (n :: acc)
+                                (List.filterMap identity [ left, right ]
+                                    ++ hs
+                                )
+            in
+            go [] [ heap ]
+
+        wrongToSortedList : Heap -> List Int
+        wrongToSortedList (Heap n left right) =
+            n
+                :: (mergeHeaps left right
+                        |> Maybe.map toList
+                        |> Maybe.withDefault []
+                   )
+
+        mergeHeaps : Maybe Heap -> Maybe Heap -> Maybe Heap
+        mergeHeaps left right =
+            case ( left, right ) of
+                ( Nothing, _ ) ->
+                    right
+
+                ( _, Nothing ) ->
+                    left
+
+                ( Just (Heap ln lleft lright), Just (Heap rn rleft rright) ) ->
+                    Just <|
+                        if ln <= rn then
+                            Heap ln (mergeHeaps lright right) lleft
+
+                        else
+                            Heap rn (mergeHeaps rright left) rleft
+    in
+    simplifiesTowards
+        "binHeap"
+        (Heap 0 Nothing (Just (Heap -1 Nothing Nothing)))
+        (heapFuzzer 4)
+        (\heap ->
+            let
+                l1 =
+                    toList heap
+
+                l2 =
+                    wrongToSortedList heap
+            in
+            (l2 == List.sort l2) && (List.sort l1 == l2)
+        )
+
+
+type Heap
+    = Heap Int (Maybe Heap) (Maybe Heap)
 
 
 {-| <https://github.com/jlink/shrinking-challenge/blob/836bafa664659a435ae186eed5b87e941228ae3d/challenges/coupling.md>
